816fd6703ef75ff24122cd58081070ddd89f97df
Michael Martinez <michael.martinez@medici.md>
2021-03-03 20:53:50 -0600

    Added fixture as source of truth

--- cypress/integration/api-component/sidebar.spec.ts
+++ cypress/integration/api-component/sidebar.spec.ts
@@ -1,41 +1,51 @@
 context('Sidebar layout', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8081/sidebar.html')
+    cy.fixture('petstore_bundled.json')
+        .then((openAPISpec) => {
+          // "this" is still the test context object
+          this.openAPISpec = openAPISpec
+        })
   })
 
   it('Page exists', () => {
     cy.get('#docs').should('exist');
   })
 
+  // How do wh
   it('API Overview is displayed when page is loaded', () => {
     //expect("TODO: 'Insert your tests here'").to.not.exist;
     cy.get('#paths').should('exist');
   })
 
+  it('API paths should be displayed at the menus top level and should be grouped by the OpenAPIs tags property', () => {
+    cy.get('#paths').should('exist');
+  })
+
   //How do we know what the overview is?
-  it('Overview page renders basic information and server information', () => {
-    cy.get('#docs').should('exist');
-  })
-
   //What server information do we need to verify?
-  it('Overview page renders server information', () => {
+  it('Overview page renders basic information and server information', () => {
+
+    const spec = this.openAPISpec
+    expect(Object.keys(spec).length).to.equal(9)
+
+    cy.get('.TableOfContentsItem--group').should('have.length', 3)
+
+
+    //cy.contains("read your pets")
+  })
+
+
+  it('Paths should be expandable and methods within paths should render summary field', () => {
     cy.get('#docs').should('exist');
   })
 
-  it('Paths should be expandable', () => {
-    cy.get('#docs').should('exist');
-  })
-
-  it('Methods within paths should render summary field', () => {
-    cy.get('#docs').should('exist');
-  })
-
-  it('models are rendered in the menu', () => {
+  it('Models are rendered in the menu', () => {
     cy.get('#docs').should('exist');
   })
 
   it('When model is selected it updates the URL location', () => {
-    cy.get('#docs').should('exist');
+    const currentUrl = cy.url();
   })
 
   // Starting with Pet because it is the most complicated,
@@ -43,8 +53,6 @@ context('Sidebar layout', () => {
   it('Pet model renderes correctly ', () => {
     cy.get('#docs').should('exist');
   })
-
-
 
 
 })
